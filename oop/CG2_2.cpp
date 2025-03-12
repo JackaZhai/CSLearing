502a32efbf57669f235ebf74583d397a60da3e9c
ZhaiJunJie <632301151811@mails.cqjtu.edu.cn>
2025-03-12 09:37:27 +0800

    完成面向对象CG两次作业 注意：部分作业未检查且未提交！！！

---
+++ oop/CG2_2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+enum CPU_Rank {P1=1, P2, P3, P4, P5, P6, P7};
+
+class CPU {
+private:
+    CPU_Rank rank;
+    int frequency;
+    float voltage;
+public:
+    CPU(CPU_Rank r) {
+        rank = r;
+        frequency = 0;
+        voltage = 0.0;
+        cout << "构造了一个CPU!" << endl;
+    }
+    ~CPU() {
+        cout << "析构了一个CPU!" << endl;
+    }
+    CPU_Rank GetRank() const { return rank; }
+    int GetFrequency() const { return frequency; }
+    float GetVoltage() const { return voltage; }
+    void SetRank(CPU_Rank r) { rank = r; }
+    void SetFrequency(int f) { frequency = f; }
+    void SetVoltage(float v) { voltage = v; }
+    void run() { 
+        cout << "CPU开始运行！" << endl;
+        cout << "等级为：" << GetRank() << endl;
+     }
+    void stop() { cout << "CPU停止运行！" << endl; }
+};
+
+int main() {
+    int r1, r2;
+    cin >> r1 >> r2;
+    
+  
+    CPU cpu1(static_cast<CPU_Rank>(r1)), cpu2(static_cast<CPU_Rank>(r2));
+    
+    cpu1.run();
+    
+    cpu1.stop();
+    
+    cpu2.run();
+    cpu2.stop();
+    
+    return 0;
+}
